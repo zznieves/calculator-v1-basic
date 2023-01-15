e143c83ba8183c7c029e4b084f74bb24c391cf84
Zayon Nieves <zayon.nieves1@gmail.com>
2023-01-15 11:32:39 -0500

    Bug Fix: fixed error with backSpace and division buttons

--- script.js
+++ script.js
@@ -78,7 +78,7 @@ function calculate() {
             answer = num1 * num2;
             break;
         // division
-        case '÷':
+        case '/':
             answer = num1 / num2;
             break;
         default:
@@ -188,7 +188,7 @@ function handleButtonClick(buttonIndex) {
         return;
     }
     // if backspace button was clicked 
-    else if(buttons[buttonIndex].textContent === '←') {
+    else if(buttons[buttonIndex].textContent === 'BS') {
 
         backSpaceClicked();
         return;
@@ -202,7 +202,7 @@ function handleButtonClick(buttonIndex) {
 
     }
     // if an operator button was clicked
-    else if((buttons[buttonIndex].textContent === '+') || (buttons[buttonIndex].textContent === '-') || (buttons[buttonIndex].textContent === 'x') || (buttons[buttonIndex].textContent === '÷')) {
+    else if((buttons[buttonIndex].textContent === '+') || (buttons[buttonIndex].textContent === '-') || (buttons[buttonIndex].textContent === 'x') || (buttons[buttonIndex].textContent === '/')) {
 
         operatorClicked(buttonIndex);
         return;
